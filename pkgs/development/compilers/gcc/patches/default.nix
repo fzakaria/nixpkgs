4c197f8f2f8cedd768ef19d7e4a18c9f226c4584
Farid Zakaria <farid.m.zakaria@gmail.com>
2025-02-12 18:40:53 -0800

    At least 13

--- pkgs/development/compilers/gcc/patches/default.nix
+++ pkgs/development/compilers/gcc/patches/default.nix
@@ -307,5 +307,6 @@ in
 ++ optional (
   (is7 || is8) && !atLeast9 && targetPlatform.libc == "musl"
 ) ./libgomp-dont-force-initial-exec.patch
-++
-[./visibility.patch]
+++ optional 
+  atLeast13
+ ./visibility.patch
